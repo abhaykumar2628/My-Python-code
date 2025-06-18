4d50f42ce0f6fcb611324d82fd6b70b673fc8451
Abhay Gupta <abhaygupta2628@gmail.com>
2025-06-18 19:38:35 +0530

    Add files via upload

---
+++ C2.py
@@ -0,0 +1,11 @@
+m1 = input("Enter Your Favourite 1st Movie :-")
+m2 = input("Enter Your Favourite 2nd Movie :-")
+m3 = input("Enter Your Favourite 3rd Movie :-")
+
+list = []
+list.insert(0, (m1))
+list.insert(1, (m2))
+list.insert(2, (m3))
+
+print("Your Favourite Movies Are",list)
+
