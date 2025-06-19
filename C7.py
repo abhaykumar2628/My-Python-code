3a9fd0d8334569b760554800d0cf5f0f212fbec8
Abhay Gupta <abhaygupta2628@gmail.com>
2025-06-19 10:37:46 +0530

    C7.py

---
+++ C7.py
@@ -0,0 +1,9 @@
+print("Make Your Voter Id")
+
+age = int(input("Enter Your Age :-"))
+
+if (age >= 18 ):
+    print("You Can Make Your Voter Id")
+    
+else :
+    print("You Cannot Make Your Voter Id Because Your Age Is Less Than 18 Years, You Must Be 18 Or Above")
